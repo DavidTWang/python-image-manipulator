15044507f427e63184b4722cc0b9ee37326a3639
godsgift <eltonsia@yahoo.com>
2015-09-21 16:28:37 -0700

    Added more comments on code and more images

--- rotation.py
+++ rotation.py
@@ -5,12 +5,16 @@ import sys
 #Grab an image and rotate it by what the user specified
 def rotate():
 	#Asks for user input about what degree they would want to rotate the image by
-	rdegrees = input('How much would you like to rotate the image by?: ')
+	imageChosen = raw_input("Image to rotate: ")
+	rdegrees = input('Degrees of rotation: ')
 	rdeg = int(rdegrees)
 	#Grab the image
-	im = Image.open("ak.jpg")
-	#Rotate the image depending on what number the user inputted and increase the size of the image to compensate weird numbers
+	im = Image.open("images/"+imageChosen)
+	#The .rotate has 3 parameters: angle, resample, and expand
+	#In this case, we use angle and expand
+	#Angle is the degrees the user wants the image rotated and expand increases the size of 
+	#the image so that it can show the whole image when rotating with wierd numbers
 	rotatedim = im.rotate(rdeg, expand=1)
-	rotatedim.save("rotatedak.jpg")
+	rotatedim.save("rotated" + imageChosen + ".jpg")
 #Main
 rotate()
