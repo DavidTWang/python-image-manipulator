e0f997912c49486c9d699b05f9e1d30e0cf74cd3
root <root@localhost.localdomain>
2015-09-25 09:33:19 -0700

    finish up blend.py

--- blend.py
+++ blend.py
@@ -20,38 +20,76 @@ def main(argv):
 		filename1 = argv[0]
 		filename2 = argv[1]
 		alpha = float(argv[2])
-		
+
 
 	# open images
 	sourceImg = open_image(filename1)
 	destImg = open_image(filename2)
 	
- 	# check the size of images if it not same, exit
-	if sourceImg.size != destImg.size:
-		print("The source image and destination image size should be same")
-		exit(1)
-
 	# check alpha value
 	if alpha < 0 or alpha > 1:
 		print("Alpha value should be >=0 or <=1")
 		exit(1)
 
-	####################################################################
-	# # blend function that takes two images and alpha
-	# The size of two images should be same to implement blend function.
-	# The last parameter 'alpha' is the blending ratio 
-	# which determines the influence of each input image in the output
-	# if alpha is 0.0, a copy of the first image is returned.
-	# if alpha is 1.0, a copy of the second image is returned. 
+	# create a result image
+	result = Image.new("RGB", sourceImg.size)
 
-	fImage = Image.blend(sourceImg, destImg, alpha)
+ 	# check the size of images if it not same, exit
+	if sourceImg.size != destImg.size:
+		print("The source image and destination image size should be same")
+		exit(1)
+
+	##############################################################################
+	# blending algorithm
+	#
+	# loop through all the pixels in source image and dest image.
+	# when it loops through, store source image pixel and dest image pixel in a  
+	# certain position (i,j).
+	# Calculate the result pixel value for the location by:
+	#		result = image1 * (1.0 - alpha) + image2 * alpha
+	# if alpha is equal to 1: only dest image shows up
+	# if alpha is equal to 0: only source image shows up.
+	#
+	##############################################################################
+
+	for i in range(0, result.width):
+		for j in range(0, result.height):
+			s = sourceImg.getpixel((i,j))
+			d = destImg.getpixel((i,j))
+			
+			r = int(s[0] * (1-alpha)) + int(d[0] * alpha)
+			g = int(s[1] * (1-alpha)) + int(d[1] * alpha)
+			b = int(s[2] * (1-alpha)) + int(d[2] * alpha)
+
+			result.putpixel((i,j), (r,g,b))
+
 	
-	####################################################################
+	result.save("blend_result.jpg")
+	result.show()
+
+
+
+	# # check alpha value
+	# if alpha < 0 or alpha > 1:
+	# 	print("Alpha value should be >=0 or <=1")
+	# 	exit(1)
+
+	# ####################################################################
+	# # # blend function that takes two images and alpha
+	# # The size of two images should be same to implement blend function.
+	# # The last parameter 'alpha' is the blending ratio 
+	# # which determines the influence of each input image in the output
+	# # if alpha is 0.0, a copy of the first image is returned.
+	# # if alpha is 1.0, a copy of the second image is returned. 
+
+	# fImage = Image.blend(sourceImg, destImg, alpha)
 	
-	# show and save the image
-	fImage.show()
-	fImage.save("blend_result.jpg")
-	fImage.close()
+	# ####################################################################
+	
+	# # show and save the image
+	# fImage.show()
+	# fImage.save("blend_result.jpg")
+	# fImage.close()
 
 
 if __name__ == '__main__':
